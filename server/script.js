637ac4e270781d126fbc775b37be4214c8280301
5935512009 <Pasut2541@hotmail.com>
2025-02-10 15:14:53 +0700

    update:{untracked script}

---
+++ server/script.js
@@ -0,0 +1,65 @@
+// {"use strict"
+//  const a = 1;
+// }
+//  b = 2;
+
+// console.log("hello");
+
+// console.log([1,2].map(num=>{
+//     if(num>0) return;
+//     return num*2;
+// }))
+
+
+
+// const num1 = 1 ;
+// console.log(typeof num1);
+
+// console.log(parseInt('1M3'));
+// console.log(parseInt('A22'));
+// console.log(parseFloat('1.3d2'));
+
+// {
+//     function abc(){
+//         console.log("aaa")
+//     }
+// }
+// abc()
+
+// function abc(){
+//     console.log(abc.xyz);
+// }
+
+// abc()
+// abc.xyz=400;
+// abc.xyz=200;
+// abc()
+
+
+// const number =[1,2,3,4];
+// number[10]=10;
+// console.log(number);
+
+// function asd(){
+//     'use strict'
+//     a =100;
+//     b =100;
+//     return args[1],args[2]
+//     console.log(a+b);
+// }
+
+// console.log(asd(300,400))
+
+// console.log(false==![]); 
+//  console.log(false==[]); 
+
+
+const arr=[1,2,3,4]
+const str="1,2,3,4"
+//     console.log(arr==str);
+
+console.log(typeof arr)
+
+// let abc = "keep"
+// abc[2] ="y"
+// console.log(aaa) 
